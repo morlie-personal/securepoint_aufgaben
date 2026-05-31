65870150d783b9a7175536a170fbce5833eb2094
Moritz Liebau <m.liebau@ostfalia.de>
2026-05-31 18:19:34 +0200

    adding file check from address_count

--- license_check.php
+++ license_check.php
@@ -1,6 +1,19 @@
 <?php
 ini_set('memory_limit', '8192M');
-$data = file("./updatev12-access-pseudonymized.log", FILE_IGNORE_NEW_LINES);
+
+$files = glob(__DIR__ . '/*.log');
+
+if (!$files) {
+echo "Keine Log Datei gefunden. Bitte Log Datei zu dem Ordner der Anwendung hinzufügen";
+error_log("Keine Log Datei gefunden. Bitte Log Datei zu dem Ordner der Anwendung hinzufügen");
+exit;
+}
+
+if (sizeof($files) > 1) {
+error_log("Mehrere Log Dateien gefunden, es kann aber nur eine geladen werden... Lade $files[0]");
+}
+
+$data = file($files[0], FILE_IGNORE_NEW_LINES);
 $serial_numbers_to_specs = array();
 
 
