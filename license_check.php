8a5d5a6d9ff29710df2c411a3a9d41e411537026
Moritz Liebau <m.liebau@ostfalia.de>
2026-05-31 19:40:10 +0200

    matching serial numbers to specs and specs to serial numbers, filtering unique specs to properly count misuses, counting the use of multiple serials per spec/specs per serial/and serial per spec without filter, echoing the result

--- license_check.php
+++ license_check.php
@@ -14,8 +14,9 @@ error_log("Mehrere Log Dateien gefunden, es kann aber nur eine geladen werden...
 }
 
 $data = file($files[0], FILE_IGNORE_NEW_LINES);
+$serial_numbers_to_specs_total = array();
 $serial_numbers_to_specs = array();
-
+$specs_to_serial_numbers = array();
 
 foreach ($data as $valueIndex => $value) {
     preg_match_all('/serial=(\S+)/', $value, $serial_nums);
@@ -23,13 +24,35 @@ foreach ($data as $valueIndex => $value) {
 
 
     $serial_numbers_to_specs[$spec[1][0]][] = $serial_nums[1][0];
+    $specs_to_serial_numbers[$serial_nums[1][0]][] = $spec[1][0];
+}
+
+foreach ($serial_numbers_to_specs as $spec => $serials) {
+    $serial_numbers_to_specs_total[$spec] = $serials;
 }
 
 foreach ($serial_numbers_to_specs as $spec => $serials) {
     $serial_numbers_to_specs[$spec] = array_unique($serials);
 }
 
+foreach ($specs_to_serial_numbers as $serial => $specs) {
+    $specs_to_serial_numbers[$serial] = array_unique($specs);
+}
 
-echo '<pre>'; print_r($serial_numbers_to_specs); echo '</pre>';
+$count = array_map('count', $specs_to_serial_numbers);
+arsort($count);
+$serial_array_total = array_slice($count, 0, -(count($count)-10));
+
+$count = array_map('count', $serial_numbers_to_specs);
+arsort($count);
+$serial_array = array_slice($count, 0, -(count($count)-10));
+
+$count = array_map('count', $specs_to_serial_numbers);
+arsort($count);
+$spec_array = array_slice($count, 0, -(count($count)-10));
+
+echo '<pre>'; print_r($serial_array_total); echo '</pre>';
+echo '<pre>'; print_r($serial_array); echo '</pre>';
+echo '<pre>'; print_r($spec_array); echo '</pre>';
 
 ?>
